3b6c90a1040c7d238a0c9d8006356e83e8f30f1f
MariaDMar <42479685+MariaDMar@users.noreply.github.com>
2019-04-30 17:13:05 -0500

    cambios en mongo precios

--- index.js
+++ index.js
@@ -6,6 +6,38 @@ const app = express();
 
 
 const motorRender = require('express-handlebars');
+
+//instalar Mongo
+var MongoClient =require('mongodb').MongoClient;
+var assert = require('assert');
+const app = express();
+//Conection URL
+const url= 'mongodb://localhost:27017';
+const dbName = 'wxmanager';
+//Create Clietn Object
+const client = new MongoClient(url);
+//connect 
+client.connect(function(err){
+assert.equal(null,err);
+console.log("Estamos conectados");
+//Conect to database
+const db=client.db(dbName);
+const productos= db.collection('productos');
+productos.find({}, {sort:['price']}).toArray(function(err,docs){
+ assert.equal(null,err);
+ console.log("encontramos atlantis");
+ console.log(docs.length);
+
+ docs.forEach(function(prod){
+     console.log(prod.price);
+ })
+});
+
+client.close();
+
+});
+
+
 //configurar la carpeta public como publica
 app.use(express.static('public'));
 
--- productos.js
+++ productos.js
@@ -4,6 +4,7 @@ module.exports = [
         name: "Usuarios",
         img: "./imagenes/precios/users.png",
         cantidad: 0,
+        price: "$20",
         
     },
 
@@ -11,27 +12,28 @@ module.exports = [
         name: "Nómina",
         img: "./imagenes/precios/nomina.png",
         cantidad: 0,
-        
+        price: "$12",
     },
 
     {
         name: "Estaciones",
         img: "./imagenes/precios/estaciones.png",
         cantidad: 0,
-        
+        price: "$40",
     },
 
     {
         name: "Transacciones",
         img: "./imagenes/precios/transacciones.png",
         cantidad: 0,
-        
+        price: "$42",
     },
 
     {
         name: "Consultoria",
         img: "./imagenes/precios/consultoria.png",
         cantidad: 0,
+        price: "$72",
         
     },
 
@@ -39,7 +41,7 @@ module.exports = [
         name: "POS",
         img: "./imagenes/precios/pos.png",
         cantidad: 0,
-        
+        price: "$122",
     },
 
 ]
