b47e3b2e13a7389464e38e31a9bcf19a3ed6745f
MariaDMar <42479685+MariaDMar@users.noreply.github.com>
2019-05-29 15:51:43 -0500

    agregando nuevas librerias greensock y aos

--- public/js/homestyle.js
+++ public/js/homestyle.js
@@ -1,9 +1,43 @@
-var mySplitText = new SplitText("#cambiarrrr", {type:"chars, words"}),
-    tl = new TimelineLite(),
-    numChars = mySplitText.chars.length;
 
-for(var i = 0; i < numChars; i++){
-  //random value used as position parameter
-  tl.from(mySplitText.chars[i], 2, {opacity:0}, Math.random() * 2);
+//var cuerpomov = document.querySelectorAll(".banner");
+
+//TweenMax.from(".banner__in", 1, {opacity: 0.3, y:-200});
+
+
+/*var tween = TweenMax.to(cuerpomov, 2, {x: '-=100', y: '+=100', onComplete: myFunction, ease: Back.easeout });
+tl.add(tween,2);*/
+
+
+/*
+TweenMax.from(cuerpomov, 2, {x:"100px"});
+
+window.onload = function(){
+  var cuerpomov = document.getElementById(".banner__in__texto");
+  TweenLite.to(cuerpomov, 2, {left:"50%"});
 }
-		
+
+var movimiento = document.querySelectorAll(".banner__in__texto");
+function moverTexto(){
+  var tl = new TweenLite();
+
+  
+}
+*/
+
+//SIRVE PARA LA PARTE DE TUTORIALES EN CONSUTRCCION
+/*var parte = document.querySelector(".nav__header__container__logo");
+console.log(parte);
+
+parte.addEventListener('click', function (event) {
+  console.log('/////// entra a TL');
+  var tl = new TimelineLite();
+  tl.to(this, 1, { y: "-200px" }, 0.7);
+  tl.play();
+});*/
+
+TweenMax.from(".banner__in__texto", 1, {opacity: 0.3, x:"-200px"});
+TweenMax.from(".banner__in__image", 1, {opacity: 0.3, x:"200px"});
+
+TweenMax.from(".caracteristicas", 1, {opacity: 0.3, y:"200px"});
+
+console.log('sirvio');
