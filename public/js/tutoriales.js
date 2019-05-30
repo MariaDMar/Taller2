1669ed6fed6569903821648cda90a5c521f76a08
MariaDMar <42479685+MariaDMar@users.noreply.github.com>
2019-05-30 18:51:21 -0500

    animacion de tutoriales terminada

--- public/js/tutoriales.js
+++ public/js/tutoriales.js
@@ -5,8 +5,9 @@ function animacionCargada(){
     var box2 = document.querySelector("#box2");
     var box3 = document.querySelector("#box3");
     var box4 = document.querySelector("#box4");
+    var box5 = document.querySelector("#box5");
     
-    var tl = new TimelineMax({repeat: -1, repeatDelay: 1});
+    var tl = new TimelineMax({repeat: -1, yoyo: true});
     
     tl.to(box4, 1, {rotation:1})
     tl.to(box4, 1, {rotation:-1})
@@ -15,8 +16,14 @@ function animacionCargada(){
 
     var tl = new TimelineMax({repeat: -1, yoyo: true});
     
-    tl.to(box3, 5, {rotation:360, transformOrigin: "center center"})
-    tl.to(box3, 5, {rotation:360, transformOrigin: "center center"})
+    tl.to(box2, 6, {rotation:360, transformOrigin: "center center"})
+    
+    var tl = new TimelineMax({repeat: -1, yoyo: true});
+    
+    tl.to(box5, 2, {x:20})
+    tl.to(box5, 2, {x:0})
+
+
 }
     
 window.addEventListener('load', animacionCargada);
